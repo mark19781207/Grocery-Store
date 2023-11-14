2051479f5b645aee81a0d44f7db154f289fe08cc
Mark Qiu <51426317+mark19781207@users.noreply.github.com>
2023-11-14 20:28:24 +1100

    m

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const Employees = require('./models/employees');
-const authMiddleware = require('./middlewares/authMiddleware');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,11 +13,13 @@ const cartsRoutes = require('./routes/cartsRoutes');
 const employeesRoutes = require('./routes/employeesRoutes');
 const ordersRoutes = require('./routes/ordersRoutes');
 const productsRoutes = require('./routes/productsRoutes');
+const loginRoutes = require('./routes/loginRoutes');
 // app.use('/groceryItems', groceryItemRoutes);
 app.use('/carts', cartsRoutes);
 app.use('/employees', employeesRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/products', productsRoutes);
+app.use('/login', loginRoutes);
 
 // Hardcoded secret key (not recommended for production)
 const secretKey = 'your_secret_key';
@@ -29,24 +29,6 @@ const users = [];
 
 app.use(bodyParser.json());
 
-// Add a user authentication route
-app.post('/login', async (req, res) => {
-  // Replace with your user authentication logic
-  const { username, password } = req.body;
-
-  // Check username and password against your database or user data
-  const employees = await Employees.find({ Username: username, Password: password });
-  if (employees.length <= 0) {
-    return res.status(401).json({ message: 'Username or password does not match!' });
-  }
-  if (employees) {
-    // Create a token
-    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-    res.json({ token, message: `Welcome ${username}` });
-  } else {
-    res.status(401).json({ message: 'Authentication failed' });
-  }
-});
 // Registration endpoint
 app.post('/register', async (req, res) => {
   const newEmployees = new Employees(req.body);
@@ -56,21 +38,6 @@ app.post('/register', async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-});
-
-// Protected route
-app.get('/protected', (req, res) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1]; // Split by space and get the second part
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-    res.json({ message: 'Protected route', user: decoded.username });
-  });
 });
 
 const PORT = process.env.PORT || 5000;
